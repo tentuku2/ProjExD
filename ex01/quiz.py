a1cd01a937df6a39a7fa73520b04ac1b447f7fa5
yamano k <c0a2113452@edu.teu.ac.jp>
2022-06-14 16:16:46 +0900

    りどみ

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,15 +1,12 @@
-import imp
-
-
 import random
 def shutudai(qnum):
     list_quiz = ["サザエの旦那の名前は？","カツオの妹の名前は？ ","タラオはカツオから見てどんな関係？"]
     print("問題:" + list_quiz[qnum])
     
 def kaito(qnum):
-    ans = input("わかるよね？＞＞")
+    anser = input("わかるよね？＞＞")
     ans_list = [["マスオ","ますお","ますおさん","アナゴ課長"],["ワカメ","わかめ"],["甥","おい","甥っ子","不倫相手"]]
-    if ans in ans_list[qnum]:
+    if anser in ans_list[qnum]:
         print("正解！君はサザエさんマスターだ！")
     else:
         print("不正解！君はちびまる子ちゃん派の用だね！")
