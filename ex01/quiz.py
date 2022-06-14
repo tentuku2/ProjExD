078e804c2210755c26eac2770754a9b932102485
yamano k <c0a2113452@edu.teu.ac.jp>
2022-06-14 16:26:57 +0900

    プログラムについて #1

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -4,9 +4,9 @@ def shutudai(qnum):
     print("問題:" + list_quiz[qnum])
     
 def kaito(qnum):
-    anser = input("わかるよね？＞＞")
+    ans = input("わかるよね？＞＞")
     ans_list = [["マスオ","ますお","ますおさん","アナゴ課長"],["ワカメ","わかめ"],["甥","おい","甥っ子","不倫相手"]]
-    if anser in ans_list[qnum]:
+    if ans in ans_list[qnum]:
         print("正解！君はサザエさんマスターだ！")
     else:
         print("不正解！君はちびまる子ちゃん派の用だね！")
