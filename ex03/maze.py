892a5366ead8ae25ab4f577ddcd4dd04d91bff7b
yamano k <c0a2113452@edu.teu.ac.jp>
2022-06-28 14:53:48 +0900

    q3

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,10 +1,12 @@
 from asyncio import events
 from cProfile import label
 from email.mime import image
+import mailcap
 import tkinter as tk
 from tkinter import Button, ttk as ttk
 import random
 import tkinter.messagebox as tkm
+import maze_maker as m_maker
 
 class Main_GUI(tk.Frame):
     def __init__(self,master):
@@ -15,11 +17,16 @@ class Main_GUI(tk.Frame):
         self.bind_all("<KeyPress>",self.key_down)
         self.bind_all("<KeyRelease>",self.key_up)
         self.tori = tk.PhotoImage(file="fig/5.png")
-        self.cx = 300
-        self.cy = 400
+        self.cx = 300 + 50
+        self.cy = 400 + 50
+        maze_lst = m_maker.make_maze(15,9)
+        m_maker.show_maze(self.maze_canvas, maze_lst)
         self.maze_canvas.create_image(self.cx,self.cy,image=self.tori,tag="tori")
+        self.main_proc()
 
-    
+    def main_proc(self):
+        self.after(100,self.main_proc)
+
     def count_up(self):
         self.tmr += 1
         self.label["text"] =  self.tmr
@@ -27,12 +34,25 @@ class Main_GUI(tk.Frame):
     
     def key_down(self,event):
         self.key = event.keysym
- 
-    def key_up(self):
+        if self.key ==  "Up":
+            self.cy-=100
+        elif self.key == "Down":
+            self.cy+=100
+        elif self.key == "Left":
+            self.cx -= 100
+        elif self.key == "Right":
+            self.cx += 100
+        else:
+            pass
+
+        print(self.cx,self.cy)
+        self.main_proc()
+
+    def key_up(self,event):
         self.key = ""
-
-
-
+        print(self.cx,self.cy)
+        self.maze_canvas.coords("tori",self.cx,self.cy)
+        self.main_proc()
 
 if __name__ == "__main__":
     root = tk.Tk()
